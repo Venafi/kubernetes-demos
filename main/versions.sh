d477df759496cba1159fd8e0a006e52f40818f24
Sitaram IYER <sitaramkm@gmail.com>
2024-05-11 21:03:48 -0500

    Update CLI (based on venctl 1.9.0), add a sample ingress and also updating versions to match venctl print-versions
    Signed-off-by: Sitaram IYER <sitaramkm@gmail.com>

--- main/versions.sh
+++ main/versions.sh
@@ -1,11 +1,11 @@
 # Versions based on output of 
 # venctl components kubernetes manifest print-versions
-export approver-policy-enterprise :="v0.15.0"
+export approver-policy-enterprise :="v0.16.0"
 export aws-privateca-issuer :="v1.2.7"
-export cert-manager :="v1.14.4"
-export cert-manager-approver-policy :="v0.13.1"
+export cert-manager :="v1.14.5"
+export cert-manager-approver-policy :="v0.14.0"
 export cert-manager-csi-driver :="v0.8.0"
-export firefly :="v1.3.3"
+export firefly :="v1.3.4"
 export trust-manager :="v0.9.2"
 export venafi-connection :="v0.0.20"
 export venafi-enhanced-issuer :="v0.13.3"
