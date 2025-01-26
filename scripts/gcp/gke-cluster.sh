2eb8461c9e4d7a608062f57534dfb99968bb2dc0
Sitaram Iyer <4009558+sitaramkm@users.noreply.github.com>
2025-01-25 18:45:42 -0600

    Add gateway resources for demo, update GKE cluster creation CLI, add Route53 record creation

--- scripts/gcp/gke-cluster.sh
+++ scripts/gcp/gke-cluster.sh
@@ -6,9 +6,10 @@ CLUSTER_NAME=${3}
 echo "Checking if cluster ${CLUSTER_NAME} exists"
 if ! gcloud container clusters describe ${CLUSTER_NAME} --region ${2} > /dev/null 2>&1; then
   echo "Creating GKE cluster"
-  gcloud container clusters create --release-channel=rapid --workload-pool=${1}.svc.id.goog ${CLUSTER_NAME} --region ${2} --num-nodes 3
+  gcloud beta container clusters create "${CLUSTER_NAME}" --region "${2}" --tier "standard" --no-enable-basic-auth --release-channel "rapid" --metadata disable-legacy-endpoints=true --num-nodes "3" --security-posture=standard --no-enable-google-cloud-access  --workload-pool "${1}.svc.id.goog" 
 else
   echo "Cluster already exists"
 fi
 echo "Configuring kubeconfig"
 gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${2}
+
